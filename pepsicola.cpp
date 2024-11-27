5e5a73960bd39a1b9114614ad501172090f9f591
AObernihin <arsenobernihin@gmail.com>
2024-11-27 21:09:36 +0200

    Добавьте файлы проекта.

---
+++ pepsicola.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+class drib
+{
+public:
+    int znamenik;
+    int chiselnik;
+
+public:
+    void setZnamenik(int _znamenik)
+    {
+        znamenik = _znamenik;
+    }
+
+    void setChiselnik(int _chiselnik)
+    {
+        chiselnik = _chiselnik;
+    }
+
+    drib operator+(const drib other) const
+    {
+        drib result;
+        result.znamenik = this->znamenik * other.znamenik;
+        result.chiselnik = this->chiselnik * other.znamenik + other.chiselnik * this->znamenik;
+        return result;
+    }
+
+    drib operator-(const drib other) const
+    {
+        drib result;
+        result.znamenik = this->znamenik * other.znamenik;
+        result.chiselnik = this->chiselnik * other.znamenik - other.chiselnik * this->znamenik;
+        return result;
+    }
+
+    drib operator*(const drib other) const
+    {
+        drib result;
+        result.znamenik = this->znamenik * other.znamenik;
+        result.chiselnik = this->chiselnik * other.chiselnik;
+        return result;
+    }
+
+    drib operator/(const drib other) const
+    {
+        drib result;
+        result.znamenik = this->znamenik * other.chiselnik;
+        result.chiselnik = this->chiselnik * other.znamenik;
+        return result;
+    }
+
+};
+
+int main()
+{
+
+
+
+}
+
+
